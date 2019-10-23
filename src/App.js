335374042cc28cb84a2e33b6ab185e83543d5177
andramanday <andra.manday@gmail.com>
2019-10-23 10:34:00 +0700

    update react js files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+// import './assets/css/style.css';
+// import Home from './pages/home/Home';
+import MainWrapper from './pages/MainWrapper';
+
+function App() {
+    return (
+        <MainWrapper/>
+    )
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './stylesheet/Home.sass'
+
+class Home extends Component{
+
+    render(){
+        return(
+            <div className="isi-content">
+                <div className="box-content">
+                    <h1>Hallo Semua <i className="fa fa-smile-wink"></i></h1>
+                    <p className="indent">Perkenalkan nama saya Andra Manday, anak ketiga dari 5 besaudara, yang berarti saya punya adik dong 2 orang, tapi 2 adik dibawah saya sudah menginggal pada saat masih bayi, pengen banget rasanya jadi seorang abang yang baik untuk adiknya, tapi tuhan berkata lain, saya dijadikan anak bungsu.</p>
+                    <p className="indent">Alhamdulillah pada bulan april tanggal 6 tahun 2019 bersyukur kepada Allah SWT saya telah menikahi teman dunia dan akhirat saya dan inshalla pada februari 2020 kami akan dikaruniai putri yang lucu, duh senengnya Waduh, Maaf ya guys malah jadi kebawa suasana, jadi aku tu orangnya biasa-biasa aja guys, cuma orang yang haus pengetahuan, dikarenakan pada saat disekolah saya bisa dibilang orang yang malas banget. tidak percaya diri slalu takut akan kemampuan sendiri, walaupun sekarang masih ada sih, dikit-dikit. haha :)</p>
+                    <p className="indent">So, Saya merasa sangat menyesal sekarang kenapa saya dulu begitu, maka dari itu saya harus mengejar ketertingalan yang sudah saya sia-siakan selama ini. dan jangan lupa bersyukur berdoa dan slalu lakukan kewajiban kita kepada Rabb kita. jadi segitu aja ya guys aku bener-bener bukan orang yang menarik</p>
+
+                    <p className="text-right">Jakarta, 18 Oktober 2019</p>
+                    <br/>
+                    <h5 className="text-right text-bold">Andra Manday</h5>
+
+                </div>
+            </div>   
+        )
+    }
+}
+
+export default Home;
